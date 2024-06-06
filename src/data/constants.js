e419c44e3d5edf0a22baae6a2adf6a1f1b2f935d
GuoxiangZ <106981818+GuoxiangZ@users.noreply.github.com>
2024-06-06 18:32:29 +0800

    initial commit

---
+++ src/data/constants.js
@@ -0,0 +1,140 @@
+export const Bio = {
+    name: "Guoxiang Zhao",
+    roles: [
+      "Student at U of T",
+      "UI Designer",
+      "Programmer",
+    ],
+    description:
+      "I am an outgoing and energetic individual who thrives on collaboration and actively seeks new challenges and opportunities to learn. While I am relatively patient, I am also adept at managing heavy workloads and am committed to tackling difficult problems methodically.",
+    github: "https://github.com/GuoxiangZ",
+    resume:
+      "https://drive.google.com/file/d/15-5rOpxDIBQewwWe9d0_dbMpZ0ONS2ID/view?usp=drive_link",
+    linkedin: "https://www.linkedin.com/in/guoxiang-zhao-186707267/",
+    insta: "https://www.instagram.com/aaron_zhgx/",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "https://logowik.com/content/uploads/images/nodejs.jpg",
+        },
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "Postgresql",
+          image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+        },
+        {
+          name: "MongoDB",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        },
+      ],
+    },
+    
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Git",
+          image:
+            "https://cdn.iconscout.com/icon/free/png-512/free-git-18-1175219.png?f=webp&w=512",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "Figma",
+          image:
+            "https://cdn4.iconfinder.com/data/icons/logos-brands-in-colors/3000/figma-logo-1024.png",
+        },
+        {
+          name: "aws",
+          image:
+            "https://www.logo.wine/a/logo/Amazon_Web_Services/Amazon_Web_Services-Logo.wine.svg",
+        },
+        {
+          name: "java",
+          image:
+            "https://cdn.iconscout.com/icon/free/png-512/free-java-60-1174953.png?f=webp&w=512",
+        },
+        {
+          name: "C",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/C_Programming_Language.svg/760px-C_Programming_Language.svg.png?20201031132917",
+        },
+      ],
+    },
+  ];
+  
+  export const experiences = [
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://banner2.cleanpng.com/20180505/eye/kisspng-university-of-toronto-higher-education-doctorate-p-5aedbbdd077178.4531088015255295650305.jpg",
+      school: "University of Toronto",
+      date: "Oct 2021 - Sep 2025",
+      desc: "I am currently enrolled in the Computer Science Specialist program and Statistics Minor at the University of Toronto, St. George campus. The courses I have taken cover a wide range of subjects including software design, computer hardware, algorithms, machine learning, introductory database concepts, data statistics and analysis, among others. These courses have provided me with a solid foundation in the field and have honed my learning abilities. Additionally, I actively participate in various extracurricular activities on campus, such as attending lectures and hackathons organized by SDSS and UT MIST, which help me stay informed about industry trends, expand my network, and continuously grow.",
+      degree: "Bachelor of Science - U of T, Computer Science & Statistics",
+    },
+    {
+      id: 1,
+      img: "https://img1.pnghut.com/10/4/5/fGsBnVd8AK/jinan-foreign-language-basic-education-text-logo.jpg",
+      school: "Jinan Foreign Language School",
+      date: "Apr 2018 - Apr 2021",
+      desc: "I completed my high school education at Jinan Foreign Language School, where I attended courses including AP Macroeconomics, AP Microeconomics, AP Chemistry, AP Statistics, AP Calculus BC, and also had my earliest exposure to computer science.",
+      degree: "High School Honor Graduate Diploma",
+    },
+    
+  ];
+  
+  export const projects = [
+  ];
+  
+  export const TimeLineData = [
+    { year: 2017, text: "Started my journey" },
+    { year: 2018, text: "Worked as a freelance developer" },
+    { year: 2019, text: "Founded JavaScript Mastery" },
+    { year: 2020, text: "Shared my projects with the world" },
+    { year: 2021, text: "Started my own platform" },
+  ];
